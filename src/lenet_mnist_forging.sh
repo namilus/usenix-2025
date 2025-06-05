7b1eeae8f6904fa35533b653c4f7c85dbcb7ea8e
Mohamed Suliman <sulimanm@tcd.ie>
2025-06-05 14:09:18 +0100

    fix error threshold

--- src/lenet_mnist_forging.sh
+++ src/lenet_mnist_forging.sh
@@ -10,7 +10,7 @@ TRACE_DIR=$(python3 generate_trace.py $MODEL $DATASET --epochs $EPOCHS --batch-s
 
 
 NUM_TO_FORGE=1
-THRESHOLD=1e-5 # taken from our results (see Fig 7a)
+THRESHOLD=1e-7 # taken from our results (see Fig 2 for Lenet/mnist b = 100)
 
 FORGE_RESULTS=$(python3 forge_across_checkpoints.py $TRACE_DIR -f $NUM_TO_FORGE --every 100 --threshold $THRESHOLD | tee /dev/tty | tail  -n 1)
 
